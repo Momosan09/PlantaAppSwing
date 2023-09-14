42e6f4f2a30e5a39f3303b2aa25284e99b467039
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2023-09-14 12:17:36 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,15 @@
+package aplicacion;
+
+
+public class Principal {
+
+	static Window estaciones, ph;
+	
+	public static void main(String [] args) {
+		System.out.println("a");
+		estaciones = new Estaciones();
+		ph = new Ph();
+	}
+	
+	
+}
